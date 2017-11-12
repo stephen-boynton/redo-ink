76e6c15d59ba1954835ba109f836f8367d50cd30
Stephen Boynton <boyntonsc@gmail.com>
2017-11-12 13:02:36 -0500

    Locate issue

--- frontend/app/src/store/index.js
+++ frontend/app/src/store/index.js
@@ -2,7 +2,7 @@ import { createStore, applyMiddleware, compose } from "redux";
 import thunk from "redux-thunk";
 
 import reducers from "./reducer";
-
+//Some issue here in the configureStore
 export default function configureStore(initialState) {
   return createStore(
     reducers,
